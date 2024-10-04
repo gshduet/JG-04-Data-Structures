0406aa04091bdef793386396b6dd8f9703ea9397
gshduet <modis2199@gmail.com>
2024-10-04 17:10:46 +0900

    auto pull test

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-   printf("Hello, World! \\n");
-   return 0;
+int mytest(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+int main()
+{
+    int c;
+    c = mytest(10, 20);
+    printf("c = %d\\n", c);
 }
